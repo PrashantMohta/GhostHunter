f00fe3f5b71c3b18283fc6788a19855aa5d9de51
Prashant Mohta <mohtaprashantrocks@gmail.com>
2022-04-23 10:20:46 +0530

    reduce float precision

--- localGhost.cs
+++ localGhost.cs
@@ -6,7 +6,10 @@ namespace GhostHunter
         public string ghostId;
         void Update(){
             var pos = transform.position;
-            GhostHunter.Instance.HkmpPipe.SendToAll(0,EVENT.UPDATE,$"{ghostId},{pos.x},{pos.y},{pos.z},{transform.localScale.x}",false,true);
+            GhostHunter.Instance.HkmpPipe.SendToAll(
+                0,EVENT.UPDATE,
+                $"{ghostId},{pos.x.ToString("0.0")},{pos.y.ToString("0.0")},{pos.z.ToString("0.0")},{transform.localScale.x.ToString("0.0")}",
+                false,true);
         }
         void OnDestroy(){
             GhostHunter.Instance.HkmpPipe.SendToAll(0,EVENT.DESTROY,ghostId,false,true);
